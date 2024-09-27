77642404830df882080bee501ff2dd873285f865
Pedroodpes <144038965+Pedroodpes@users.noreply.github.com>
2024-09-27 09:01:38 -0300

    Add files via upload

---
+++ perguntaszz-main/Flashcards-main/perguntas.js
@@ -0,0 +1,42 @@
+criaCartao(
+    'futebol',
+    'Quem é o maior artilheiro das seleçoes?',
+    'CRISTIANO RONALDOOO, CR7'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Alemanha?',
+    'Berlin'
+)
+
+criaCartao(
+    'Historia',
+    'Em que ano a segumda guerra mundial acabou?',
+    '1945'
+)
+
+criaCartao(
+    'Inglês',
+    'Como se diz mercado em inglês?',
+    'Market'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é a raiz quadrada de 484?',
+    '22'
+)
+
+criaCartao(
+    'Química',
+    'O que são protons e eletrons?',
+    'protons são cargas positivas e eletrons são cargas negativas'
+)
+
+criaCartao(
+    'Politica',
+    'Quantos presidentes da constituição Brasileira foram presos no século 21?',
+    'luiz inacio lula da silva e michel temer'
+)
+
